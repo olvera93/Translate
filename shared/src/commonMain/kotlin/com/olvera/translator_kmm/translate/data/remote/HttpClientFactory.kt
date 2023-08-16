91e54710b458fa9ac84b85dba6ea815d13dfbdcf
olvera93 <olvera9302@outlook.com>
2023-08-15 18:52:44 -0600

    [ADD] Setting up the Translation Client

---
+++ shared/src/commonMain/kotlin/com/olvera/translator_kmm/translate/data/remote/HttpClientFactory.kt
@@ -0,0 +1,8 @@
+package com.olvera.translator_kmm.translate.data.remote
+
+import io.ktor.client.HttpClient
+
+expect class HttpClientFactory {
+    fun create(): HttpClient
+
+}
---
+++ shared/src/commonMain/kotlin/com/olvera/translator_kmm/translate/domain/translate/TranslateClient.kt
@@ -0,0 +1,12 @@
+package com.olvera.translator_kmm.translate.domain.translate
+
+import com.olvera.translator_kmm.core.domain.language.Language
+
+interface TranslateClient {
+
+    suspend fun translate(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): String
+}
