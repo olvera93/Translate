ab99be15344150d8beca978adf0c5c31ceff6296
olvera93 <olvera9302@outlook.com>
2023-08-16 18:19:13 -0600

    [ADD] Setting up the History Database

---
+++ shared/src/commonMain/kotlin/com/olvera/translator_kmm/translate/domain/history/HistoryDataSource.kt
@@ -0,0 +1,8 @@
+package com.olvera.translator_kmm.translate.domain.history
+
+import com.olvera.translator_kmm.core.domain.util.CommonFlow
+
+interface HistoryDataSource {
+    fun getHistory(): CommonFlow<List<HistoryItem>>
+    suspend fun insertHistoryItem(item: HistoryItem)
+}
---
+++ shared/src/commonMain/kotlin/com/olvera/translator_kmm/translate/domain/history/HistoryItem.kt
@@ -0,0 +1,12 @@
+package com.olvera.translator_kmm.translate.domain.history
+
+import com.olvera.translator_kmm.core.domain.language.Language
+
+data class HistoryItem(
+    val id: Long?,
+    val fromLanguageCode: String,
+    val fromText: String,
+    val toLanguageCode: String,
+    val toText: String
+
+)
